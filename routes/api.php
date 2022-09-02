8b5832c7bad3b89f9b2ee1991274ff41d7c47ea8
FreitasLucasM <lmrfreitas9@gmail.com>
2022-09-02 18:27:38 -0300

    api crud completa

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtigoController;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// List artigos
+Route::get('artigos', [ArtigoController::class, 'index']);
+
+// List single artigo
+Route::get('artigo/{id}', [ArtigoController::class, 'show']);
+
+// Create new artigo
+Route::post('artigo', [ArtigoController::class, 'store']);
+
+// Update artigo
+Route::put('artigo/{id}', [ArtigoController::class, 'update']);
+
+// Delete artigo
+Route::delete('artigo/{id}', [ArtigoController::class, 'destroy']);
